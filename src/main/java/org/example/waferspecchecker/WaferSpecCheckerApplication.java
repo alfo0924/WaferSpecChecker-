a9e2ed30da60f628fab37135f6285a48d36c1fb4
alfo0924 <halo0924@yahoo.com.tw>
2025-05-29 23:46:20 +0800

    v1.01 initial files upload

---
+++ src/main/java/org/example/waferspecchecker/WaferSpecCheckerApplication.java
@@ -0,0 +1,13 @@
+package org.example.waferspecchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaferSpecCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaferSpecCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/waferspecchecker/WaferSpecCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.waferspecchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaferSpecCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
